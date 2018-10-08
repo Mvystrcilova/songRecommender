6b9460ee9b04b0ba0665848d4f748f913ca96ea0
Michaela Vystrcilova <Mvystrcilova1@seznam.cz>
2018-10-08 12:08:12 +0200

    my_lists fungujou

--- songRecommender/views.py
+++ songRecommender/views.py
@@ -7,14 +7,14 @@ from itertools import chain
 
 # Create your views here.
 
-class HomePageView(generic.ListView):
+class HomePageView(LoginRequiredMixin, generic.ListView):
     context_object_name = 'home_list'
     queryset = Song.objects.all()
     template_name = 'songRecommender/index.html'
 
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
-        context['my_lists'] = List.objects.all()
+        context['my_lists'] = List.objects.filter(user_id=self.request.user)
         return context
 
 
@@ -40,10 +40,17 @@ class ListDetailView(LoginRequiredMixin, generic.DetailView):
 class MyListsView(LoginRequiredMixin, generic.ListView):
     model = List
     template_name = 'songRecommender/my_lists.html'
+    context_object_name = 'moje_listy'
     paginate_by = 10
 
     def get_queryset(self):
         return List.objects.filter(user_id=self.request.user)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(MyListsView, self).get_context_data(**kwargs)
+        context['played_songs'] = Played_Song.objects.filter(user_id=self.request.user)
+
+        return context
 
 class RecommendedSongsView(generic.ListView):
     model = Song
